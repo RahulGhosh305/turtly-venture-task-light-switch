bb15861ee7917e73169905e4fc86e0fe3bec873c
Rahul Ghosh <rahulghosh096@gmail.com>
2022-05-25 13:55:29 +0600

    Remove external files and rearrange internal codes and project done

--- script.js
+++
@@ -1,8 +0,0 @@
-const toggleImage = () => {
-    const image = document.getElementById("myLight")
-    if (image.src.match("finalLightOff")) {
-        image.src = "./assets/finalLightOn.png";
-    } else {
-        image.src = "./assets/finalLightOff.png";
-    }
-}
